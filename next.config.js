be562edfd53c153815aaf61c338ec1a05c78477e
arshaan-abh <arshaan.git@gmail.com>
2023-08-03 13:55:53 +0330

    Add globals.scss

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path')
+
+const nextConfig = {
+    webpack: (config) => {
+        config.resolve.alias['@src'] = path.join(__dirname, 'src')
+        return config
+    }
+}
 
 module.exports = nextConfig
